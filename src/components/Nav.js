5524f387b9562d11773aeb01d73ea09b3bf51811
kurtreyn <kurtreyn@yahoo.com>
2024-01-08 17:37:21 -0500

    add netlify.toml & redirects

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from '../routes/ProtectedRoute';
-// import AuthRoute from '../routes/AuthRoute';
 import Login from '../pages/Login';
 import Home from '../pages/Home';
 
 export default function Nav() {
   return (
     <Routes>
-      {/* <Route element={<AuthRoute currentUser={currentUser} />}>
-        <Route element={<Home />} path="/" exact />
-      </Route> */}
       <Route
         path="/"
         element={
