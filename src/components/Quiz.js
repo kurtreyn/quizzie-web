4c1bbfeb40564f25f718c1f9ae300465ee5407e5
kurtreyn <kurtreyn@yahoo.com>
2024-09-11 21:00:18 -0400

    update Quiz

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -119,6 +119,7 @@ export default function Quiz({ subjectName, group }) {
     if (longestAnswer > 100) {
       setLongAnswerMode(true);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [disabled, longestAnswer]);
 
   return (
